8f7b5e37864affb1c1346ac39d8c01134eaec8ce
Levi Donaldson <lcdbacanas@gmail.com>
2020-02-26 20:47:16 -0600

    fixing as I go

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 
+get_todos = () => {
+    let todos = new Array;
+    let todos_str = localStorage.getItem('todo');
+    if (todos_str != null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+
+onSearch = () => {
+    let inputVal = document.getElementById("searchInput").value;
+
+    let todos = get_todos();
+    todos.push(inputVal);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+    // let result = document.getElementById("displaySearch").innerHTML=inputVal;
+    return result;
+}
+
+show = () => {
+    let todos = get_todos();
+
+    let html = '<ul>';
+    for (let i = 0; i < todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('displaySearch').innerHTML = html;
+
+    let buttons = document.getElementsByClassName('remove');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+remove = () => {
+    let id = this.getAttribute('id');
+    let todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
